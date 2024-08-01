9ed3512dbcb22a29cbbace8f75a98934fc1f2217
Fiznwahid <152114008+Fiznwahid@users.noreply.github.com>
2024-08-01 15:58:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var message = document.getElementById('message');
+
+    // Validasi login sederhana
+    if (username === 'Intan' && password === '24 Agustus 2024') {
+        message.style.color = 'green';
+        message.textContent = 'Login berhasil!';
+
+        // Arahkan pengguna ke halaman berikutnya
+        window.location.href = 'https://fiznwahid.github.io/LOVEYOU/';  // Ganti URL ini dengan alamat tujuan yang diinginkan
+    } else {
+        message.style.color = 'red';
+        message.textContent = 'Username atau password salah!';
+    }
+});
