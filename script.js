e93ff2493fe5018a672be587e73c46e93f323ac9
Fiznwahid <152114008+Fiznwahid@users.noreply.github.com>
2024-08-01 16:04:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var message = document.getElementById('message');
 
     // Validasi login sederhana
-    if (username === 'XXXXX' && password === '24 Agustus 2024') {
+    if (username === 'XXXXX' && password === '24Agustus2024') {
         message.style.color = 'green';
         message.textContent = 'Login berhasil!';
 
